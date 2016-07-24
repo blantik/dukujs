68bd27ffe83634b554250860bd2547851ac2e12b
Shodiqul Muzaki <user.creativefull@gmail.com>
2016-07-25 00:53:39 +0700

    clear content

--- App/routes.js
+++ App/routes.js
@@ -2,6 +2,6 @@
 module.exports = exports = function (app) {
 	// Handler Server
 	app.get("/", function(req,res,next) {
-		res.send("Hello, this is duku core", a);
+		res.send("Hello, this is duku core");
 	})
 }
